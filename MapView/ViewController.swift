51a50df8116de28607a899aea744cd954c059cea
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-06-07 12:26:12 +0900

    map_view

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MapView
 //
-//  Created by D7703_26 on 2018. 6. 7..
+//  Created by D7703_25 on 2018. 6. 7..
 //  Copyright © 2018년 dit. All rights reserved.
 //
 
@@ -19,21 +19,23 @@ class ViewController: UIViewController {
         //위도, 경도 설정
         //DIT 부산광역시 부산진구 양정동 429-19
         //35.165999, 129.072543
+        
         let loc: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 35.165999, longitude: 129.072543)
+        
         //변경
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         
         //region
         let region = MKCoordinateRegion(center: loc, span: span)
         
-        //mapkit에 설정
+        //맵킷에 설정
         myMapView.setRegion(region, animated: true)
         
-        // annotation 끊기
+        // 어노테이션 끊기
         let anno = MKPointAnnotation()
         anno.coordinate = loc
         anno.title = "DIT 동의과학대학교"
-        anno.subtitle = "내꿈이 자라는 곳"
+        anno.subtitle = "내꿈이 자라는 곳~!"
         myMapView.addAnnotation(anno)
     }
 
